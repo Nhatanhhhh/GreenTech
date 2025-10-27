f2b962221c9f58fe8d4f297c990b110b028c705d
SOLPTT <0858085036tan@gmail.com>
2025-10-28 00:40:23 +0700

    Add View List All Reviews
    new feature View List All Reviews

---
+++ BLL/Service/AdminReviewService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BLL.Service.Interface;
+using DAL.Context;
+using DAL.Models;
+
+namespace BLL.Service
+{
+    public class AdminReviewService : IAdminReviewService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminReviewService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private bool IsAdminRole(int userId)
+        {
+            var roleName = (from ur in _context.UserRoles
+                            join r in _context.Roles on ur.RoleId equals r.Id
+                            where ur.UserId == userId
+                            select r.RoleName.ToString())
+                            .FirstOrDefault();
+
+            return roleName == "ROLE_ADMIN";
+        }
+
+        public List<Review> GetAllReviews(int adminUserId)
+        {
+            if (!IsAdminRole(adminUserId))
+                return new List<Review>(); // Không có quyền -> trả về rỗng
+
+            var reviews = _context.Reviews
+                .OrderByDescending(r => r.CreatedAt)
+                .ToList();
+
+            return reviews;
+        }
+    }
+}
---
+++ BLL/Service/Interface/IAdminReviewService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace BLL.Service.Interface
+{
+    public interface IAdminReviewService
+    {
+        List<Review> GetAllReviews(int adminUserId);
+    }
+}
